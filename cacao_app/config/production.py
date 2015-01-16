ad13a886a386d6ee9dea44c800bde5436adb381b
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2015-01-15 22:13:19 -0600

    dasdfadsf

--- cacao_app/config/production.py
+++ cacao_app/config/production.py
@@ -87,13 +87,13 @@ class Production(Common):
 
     # EMAIL
     DEFAULT_FROM_EMAIL = values.Value('cacao_app <noreply@cacao.kronoscode.com>')
-    EMAIL_HOST = values.Value('smtp.sendgrid.com')
-    EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
-    EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
-    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
-    EMAIL_SUBJECT_PREFIX = values.Value('[cacao_app] ', environ_name="EMAIL_SUBJECT_PREFIX")
-    EMAIL_USE_TLS = True
-    SERVER_EMAIL = EMAIL_HOST_USER
+    #EMAIL_HOST = values.Value('smtp.sendgrid.com')
+    #EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
+    #EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
+    #EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
+    #EMAIL_SUBJECT_PREFIX = values.Value('[cacao_app] ', environ_name="EMAIL_SUBJECT_PREFIX")
+    #EMAIL_USE_TLS = True
+    #SERVER_EMAIL = EMAIL_HOST_USER
     # END EMAIL
 
     # TEMPLATE CONFIGURATION
